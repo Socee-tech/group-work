9ffc16ac3c3b461c6fa0a59c23e32dab8006a36f
Socee-tech <sospeterngeresa@gmail.com>
2025-04-04 09:35:04 +0300

    initial commit

---
+++ game_park/script.js
@@ -0,0 +1,40 @@
+// Placeholder for future JS
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("WildHaven Game Park website loaded.");
+  });
+
+  if (window.location.pathname.includes("about.html")) {
+    console.log("About page loaded");
+  }
+
+  if (window.location.pathname.includes("wildlife.html")) {
+    console.log("Wildlife page loaded");
+  }
+
+  if (window.location.pathname.includes("activities.html")) {
+    console.log("Activities page loaded");
+  }
+
+  if (window.location.pathname.includes("accommodation.html")) {
+    console.log("Accommodation page loaded");
+  }
+
+  if (window.location.pathname.includes("restaurant.html")) {
+    console.log("Restaurant page loaded");
+  }
+
+  if (window.location.pathname.includes("gallery.html")) {
+    console.log("Gallery page loaded");
+  }
+
+  if (window.location.pathname.includes("plan.html")) {
+    console.log("Plan Your Visit page loaded");
+  }
+
+  if (window.location.pathname.includes("booking.html")) {
+    console.log("Booking page loaded");
+  }
+
+  if (window.location.pathname.includes("contact.html")) {
+    console.log("Contact Us page loaded");
+  }
